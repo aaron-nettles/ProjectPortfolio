ecca30b85ae7b9be27858eb2c428ad73035583a7
Nettles <aaron.i.nettles@gmail.com>
2023-02-09 12:51:24 -0600

    changes made to pacman files

--- Games/C#/unity-pacman/Assets/Scripts/PowerPellet.cs
+++ Games/C#/unity-pacman/Assets/Scripts/PowerPellet.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PowerPellet : Pellet
 {
     // The duration of the power effect which can be changed to make the level easier!
-    public float duration = 16f;
+    //This is overrided in the GhostFrightened script, so the changes must be made there
+    public float duration = 8f;
 
     // Override the `Eat` method from the base class
     protected override void Eat()
